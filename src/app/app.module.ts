209322461229f46c28734fbe3fb88132504e44f3
Rodrigo <rodrigoflima@ua.pt>
2021-05-25 15:39:52 +0100

    workout cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,25 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyWorkoutComponent } from './my-workout/my-workout.component';
 import { HomeComponent } from './home/home.component';
+import { WorkoutCardComponent } from './workout-card/workout-card.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  declarations: [AppComponent, NavigationBarComponent, MyWorkoutComponent, HomeComponent],
+  declarations: [AppComponent, NavigationBarComponent, MyWorkoutComponent, HomeComponent, WorkoutCardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
     BrowserAnimationsModule,
+    // Modules for the workout cards
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
