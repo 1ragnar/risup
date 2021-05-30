cd79d662eade7e06b78ce4c62f92e0c70cc40efd
Rodrigo <rodrigoflima@ua.pt>
2021-05-30 15:10:41 +0100

    added content on workout-cards modals

--- src/app/workout-card/workout-card.component.ts
+++ src/app/workout-card/workout-card.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit, Inject } from '@angular/core';
 import { Workout } from '../workout';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
 
 export interface DialogData {
   name: string;
   difficulty: string;
+  video: string;
+  description: string;
 }
 
 @Component({
@@ -16,18 +19,21 @@ export class WorkoutCardComponent implements OnInit {
 
   name: string;
   difficulty: string;
+  video: string;
+  description: string;
 
-  constructor(public dialog: MatDialog) {}
-
+  constructor(public dialog: MatDialog, private _sanitizer: DomSanitizer) {}
+ 
   ngOnInit(): void {}
 
   @Input() data: Workout;
 
   openWorkout(): void {
     const dialogRef = this.dialog.open(workoutModal, {
-      width: '500px',
+      width: '1000px',
       data: {name: this.data.name, difficulty: this.data.difficulty}
     });
+    
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
     });
